d3ffecf8d6f94c40f485da954a3304abb2aa0dc2
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-25 13:50:59 +0200

    upload: add HTTP endpoint src/Http/Controller/Upload/UploadChunkController.php

---
+++ src/Http/Controller/Upload/UploadChunkController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mediarama\Http\Controller\Upload;
+
+use Mediarama\Upload\Application\ReceiveUploadChunk;
+use Mediarama\Upload\Domain\UploadChunk;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Uid\Uuid;
+
+final readonly class UploadChunkController
+{
+    public function __construct(private ReceiveUploadChunk $receive)
+    {
+    }
+
+    #[Route('/api/uploads/{id}/chunks/{index}', name: 'upload_chunk', methods: ['PUT'])]
+    public function __invoke(string $id, int $index, Request $request): JsonResponse
+    {
+        $userId = Uuid::fromString((string) $request->headers->get('X-Mediarama-User'));
+        $size = (int) $request->headers->get('Content-Length', '0');
+        $offset = (int) $request->headers->get('Upload-Offset', '0');
+        $checksum = strtolower((string) $request->headers->get('Upload-Checksum-SHA256', ''));
+
+        $stream = fopen('php://input', 'rb');
+        if ($stream === false) {
+            throw new \RuntimeException('Unable to read upload request body.');
+        }
+
+        ($this->receive)(
+            Uuid::fromString($id),
+            $userId,
+            new UploadChunk($index, $offset, $size, $checksum),
+            $stream,
+        );
+
+        fclose($stream);
+
+        return new JsonResponse(['accepted' => true], 202);
+    }
+}
