a1e90107643275a95e7575cc66c288672ee68b6f
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-25 15:26:29 +0200

    import: migrate Coppermine collection ACL src/Import/Coppermine/CoppermineAclImportReport.php

---
+++ src/Import/Coppermine/CoppermineAclImportReport.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mediarama\Import\Coppermine;
+
+final readonly class CoppermineAclImportReport
+{
+    /** @param list<string> $unmappedPrincipals
+     *  @param list<string> $passwordResetAlbums
+     */
+    public function __construct(
+        public int $processedAlbums,
+        public int $createdAccessRules,
+        public array $unmappedPrincipals,
+        public array $passwordResetAlbums,
+    ) {
+    }
+}
