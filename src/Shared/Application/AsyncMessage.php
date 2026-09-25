bd180c46305c4e26eff94658d9069aea4bc2d9f9
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-25 13:25:02 +0200

    foundation: add src/Shared/Application/AsyncMessage.php

---
+++ src/Shared/Application/AsyncMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mediarama\Shared\Application;
+
+/**
+ * Marker interface for application messages that must be handled asynchronously.
+ */
+interface AsyncMessage
+{
+}
