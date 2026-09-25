668001a8f49647e181c5a2db6fb9c784de35b3f9
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-25 14:36:08 +0200

    security: move upload controllers behind CurrentUser boundary

--- src/Http/Controller/Upload/FinalizeUploadController.php
+++ src/Http/Controller/Upload/FinalizeUploadController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Uid\Uuid;
 
 final readonly class FinalizeUploadController
 {
-    public function __construct(private FinalizeUpload $finalize)
+    public function __construct(private FinalizeUpload $finalize, private CurrentUser $currentUser)
     {
     }
 
@@ -21,7 +21,7 @@ final readonly class FinalizeUploadController
     {
         $media = ($this->finalize)(
             Uuid::fromString($id),
-            Uuid::fromString((string) $request->headers->get('X-Mediarama-User')),
+            $this->currentUser->requireUser()->id,
         );
 
         return new JsonResponse([
