e65adcbf47ce1cd8d2da6ff2717a3edf32d1ee25
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-25 14:36:30 +0200

    fix: import CurrentUser in upload controller

--- src/Http/Controller/Upload/UploadChunkController.php
+++ src/Http/Controller/Upload/UploadChunkController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mediarama\Http\Controller\Upload;
 
+use Mediarama\Security\Application\CurrentUser;
 use Mediarama\Upload\Application\ReceiveUploadChunk;
 use Mediarama\Upload\Domain\UploadChunk;
 use Symfony\Component\HttpFoundation\JsonResponse;
