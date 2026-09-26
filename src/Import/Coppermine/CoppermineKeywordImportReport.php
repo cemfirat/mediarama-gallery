ee4211cb33f8d71307ce9ce8b5614e58412d6175
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-26 08:41:25 +0200

    import: report linked Coppermine album memberships

--- src/Import/Coppermine/CoppermineKeywordImportReport.php
+++ src/Import/Coppermine/CoppermineKeywordImportReport.php
@@ -6,11 +6,16 @@ namespace Mediarama\Import\Coppermine;
 
 final readonly class CoppermineKeywordImportReport
 {
-    /** @param list<string> $unmappedPictureIds */
+    /**
+     * @param list<string> $unmappedPictureIds
+     * @param list<string> $unmappedAlbumIds
+     */
     public function __construct(
         public int $createdTags,
         public int $createdLinks,
+        public int $linkedAlbumMembershipsImported,
         public array $unmappedPictureIds,
+        public array $unmappedAlbumIds,
         public string $separator,
     ) {
     }
